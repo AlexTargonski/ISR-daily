92bb828313ae8479bd5cd04a2cfc0f360bfe6930
Oleksandr <targonsky97@gmail.com>
2019-02-14 15:56:28 +0200

    feat: add home page

---
+++ API.js
@@ -0,0 +1,38 @@
+export const NEWS = [
+  {
+    id : 1,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://stmed.net/sites/default/files/abstract-wallpapers-25162-4853075.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+  {
+    id : 3,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/keyboard-debra-hurd.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+  {
+    id : 4,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://stmed.net/sites/default/files/abstract-wallpapers-25162-4853075.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+  {
+    id : 5,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://stmed.net/sites/default/files/abstract-wallpapers-25162-4853075.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+  {
+    id : 6,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://stmed.net/sites/default/files/abstract-wallpapers-25162-4853075.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+  {
+    id : 7,
+    title : 'Lorem Ipsum is simply dummy text',
+    photo : 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/keyboard-debra-hurd.jpg',
+    desc  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'
+  },
+];
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { NEWS }             from '../API';
+
+import NewsCard from '../components/NewsCard';
+
+const HomePage = () => (
+  <div>
+    <h2 className="h1-responsive font-weight-bold text-center my-5">Recent posts</h2>
+    <p className="text-center w-responsive mx-auto mb-5">
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+    sunt in culpa qui officia deserunt mollit
+    anim id est laborum.
+    </p>
+    {NEWS.map(n =>
+      <NewsCard
+        key={n.id}
+        title={n.title}
+        photo={n.photo}
+        desc={n.desc}
+      />
+    )}
+  </div>
+)
+
+export default HomePage;
